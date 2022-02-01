f6a6f0c9640b7375e4e6955db35fa8d901a02a60
Vinay-96 <89307567+Vinay-96@users.noreply.github.com>
2022-02-01 20:51:44 +0530

    Id Update

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -36,8 +36,8 @@ const Cryptocurrencies = ({ simplified }) => {
 
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
-          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-            <Link to={`/crypto/${currency.id}`}>
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
+            <Link to={`/crypto/${currency.uuid}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
                 extra={
@@ -49,9 +49,9 @@ const Cryptocurrencies = ({ simplified }) => {
                 }
                 hoverable
               >
-                <p>Price:{millify(currency.price)}</p>
-                <p>Market Cap:{millify(currency.marketCap)}</p>
-                <p>Daily Change:{millify(currency.change)}%</p>
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}%</p>
               </Card>
             </Link>
           </Col>
